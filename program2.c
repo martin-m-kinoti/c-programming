8a4578f77267b432754544fdc2228b163e039ad5
martin-m-kinoti <kinotimartincs095@gmail.com>
2024-09-22 19:57:43 +0300

    sample program2

---
+++ program2.c
@@ -0,0 +1,17 @@
+// list of any 5 numbers
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+    int x, numbers[5];
+    printf("Enter any 5 numbers:\n");
+    for (x=0; x <=4; x++) {
+        scanf("%d\n", &numbers[x]);
+    }
+
+    // change last value to 20
+    numbers[4] = 20;
+    printf("updated value:\t%d\n", numbers[4]);
+    
+    return 0;
+}
